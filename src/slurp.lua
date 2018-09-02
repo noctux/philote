32ec3f0f5fad5d8fbf2e251da7a97161e6fcb9a1
Piotr Śliwka <contact@psliwka.info>
2018-09-02 13:44:53 +0200

    Minor `slurp` improvement

--- src/slurp.lua
+++ src/slurp.lua
@@ -5,7 +5,7 @@ local base64  = require("base64")
 
 function main(arg)
 	local module = Ansible.new({
-		src = { required=true, aliases={"path"} },
+		src = { required=true, type="path", aliases={"path"} },
 	})
 
 	module:parse(arg[1])
